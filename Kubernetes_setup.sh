c7fe7299104608c2533b45b220533ab1b868566b
RafaRuiz-Clavijo <rruizclavijo@miuandes.cl>
2022-10-05 19:36:37 -0300

    fixing api

--- Kubernetes_setup.sh
+++ Kubernetes_setup.sh
@@ -9,3 +9,6 @@ docker push $1/nuxt_dev2
 
 docker tag proyect_startup-api $1/proyect_startup-api
 docker push $1/proyect_startup-api
+
+docker tag proyect_startup-nginx $1/proyect_startup-nginx
+docker push $1/proyect_startup-nginx
