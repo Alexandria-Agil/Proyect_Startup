6fd94192fe32d55168f5bb10cb25f07cca074b9d
RafaRuiz-Clavijo <rruizclavijo@miuandes.cl>
2022-10-16 20:47:55 -0300

    kubernets base for airflow

--- dags/Verify_Pdf.py
+++ dags/Verify_Pdf.py
@@ -6,15 +6,23 @@ from datetime import datetime, timedelta
 TITLE_MAX_CHAR = 100
 DESC_MAX_CHAR = 500
 
-def pdf_filter(ti, **context):
+def pdf_filter_title(ti, **context):
     title = context['dag_run'].conf['title']
-    desc = context['dag_run'].conf['description']
-    if len(title) > TITLE_MAX_CHAR:
+    if len(title) == 0:
+        raise ValueError(f"You have to write a title for your PDF")
+    elif len(title) > TITLE_MAX_CHAR:
         raise ValueError(f"The title is too large, has {len(title)} chars (MAX: {TITLE_MAX_CHAR})")
-    elif len(desc) > DESC_MAX_CHAR:
-        raise ValueError(f"The description is too large, has {len(desc)} chars (MAX: {DESC_MAX_CHAR})")
     return 0
-    
+
+def pdf_filter_description(ti, **context):
+    try:
+        desc = context['dag_run'].conf['title']
+    except:
+        desc = ""
+    if len(desc) > DESC_MAX_CHAR:
+        raise ValueError(f"The title is too large, has {len(desc)} chars (MAX: {DESC_MAX_CHAR})")
+    return 0
+
 args = {
     'owner': 'brock',
     'depends_on_past': False,
@@ -34,6 +42,11 @@ with DAG(
     tags=["Filter"]
 ) as dag:
     task1 = PythonOperator(
-        task_id="pdf_filter",
-        python_callable = pdf_filter
+        task_id="pdf_filter_title",
+        python_callable = pdf_filter_title
     )
+    task2 = PythonOperator(
+        task_id="pdf_filter_description",
+        python_callable = pdf_filter_description
+    )
+    task1 >> task2
