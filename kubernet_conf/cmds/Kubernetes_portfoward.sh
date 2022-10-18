65f6cafa1f1af6db255b383b06913f06506d3d49
RafaRuiz-Clavijo <rruizclavijo@miuandes.cl>
2022-10-17 23:21:28 -0300

    'fixing' airflow on kubernetes

---
+++ kubernet_conf/cmds/Kubernetes_portfoward.sh
@@ -0,0 +1,1 @@
+kubectl port-forward service/nuxt 3001:3001 & kubectl port-forward service/nuxt2 3002:3002 & kubectl port-forward service/api 5000:5000 & kubectl port-forward service/db 5433:5433 & kubectl port-forward service/nginx 9000:9000 & kubectl port-forward service/airflow-webserver 8080:8080
