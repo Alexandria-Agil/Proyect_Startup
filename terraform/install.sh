614fb781a6dfdab4b84c2040321b3f410e16622b
RafaRuiz-Clavijo <rruizclavijo@miuandes.cl>
2022-11-07 18:25:03 -0300

    terraform modified

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-systemctl enable docker
-systemctl start docker
-docker run -dit -p 8080:8080 rafaruizc/proyect_startup_fastapi
+sudo yum update -y
+sudo yum search docker
+sudo yum info docker
+sudo yum install docker -y
+sudo usermod -a -G docker ec2-user
+id ec2-user
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+docker run -dit -p 8000:8000 rafaruizc/proyect_startup_fastapi
--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -5,6 +5,19 @@ resource "aws_instance" "server1" {
   subnet_id = aws_subnet.public-subnet.id
   security_groups = ["${aws_security_group.instancesg.id}"]
   associate_public_ip_address = true
+  provisioner "remote-exec" {
+    connection {
+        host = self.public_ip
+        type = "ssh"
+        user = "ec2-user"
+        private_key = tls_private_key.Web-Key.private_key_pem
+        timeout = "1m"
+        agent = "false"
+      }
+    inline = [
+      "sudo sed -i -e '$a\\' -e '${aws_instance.server1.private_ip} server1' /etc/hosts"
+    ]
+  } 
   user_data = file("install.sh")
 
   tags = {
